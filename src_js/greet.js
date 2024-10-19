5d1f21c28814e9bee15d8c07a9defb046c76b307
Zoey <43305755+zperson50@users.noreply.github.com>
2024-10-19 11:08:55 -0700

    Update greet.js

--- src_js/greet.js
+++ src_js/greet.js
@@ -17,6 +17,6 @@ function generateGreeting(name) {
 }
 
 // Set the greeting in the HTML element with the id "greeting"
-const name = "Hitarth"; // Replace with the desired name
+const name = "Zoey"; // Replace with the desired name
 const greetingElement = document.getElementById("greeting");
 greetingElement.textContent = generateGreeting(name);
